3900451fbcfadd7a07bb5ac8e4582e18e9a24713
Nizam Uddin Tuhin <nizamddin2001@gmail.com>
2024-03-06 17:57:45 +0600

    Update EmploymentViewModel.cs

--- StarTech.Model/HR/EmploymentViewModel.cs
+++ StarTech.Model/HR/EmploymentViewModel.cs
@@ -9,16 +9,18 @@ namespace StarTech.Model.HR
  
     public class EmploymentViewModel : EmploymentModel
     {
-        //public string EmpName { get; set; }
         public int? Gender { get; set; }
         public string BusinessNature { get; set; }
         public string Department { get; set; }
         public string Designation { get; set; }
         public string GradeName { get; set; }
-        //public int? GradeValue { get; set; }
         public string ProjectName { get; set; }
         public string CompanyName { get; set; }
         public string CompanyLocation { get; set; }
         public string CompanyAddress { get; set; }
+        public string EmpMail { get; set; }
+        public string RecommendToEmail { get; set; }
+        public string ReportToEmail { get; set; }
+
     }
 }
